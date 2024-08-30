e4314a66bbfe702217c8de4cd76d0f19ca9abb2a
Christopher David <chris@openagents.com>
2024-08-30 00:19:07 -0500

    convert to npub, show on home

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,16 +1,19 @@
 import "text-encoding-polyfill"
 import { StatusBar } from "expo-status-bar"
-import { Image, StyleSheet, View } from "react-native"
+import { nip19 } from "nostr-tools"
+import { Image, LogBox, StyleSheet, Text, View } from "react-native"
 import { useStore } from "@/lib/store"
 import { useNostrUser } from "./lib/useNostrUser"
 
+LogBox.ignoreLogs(["Promise"])
+
 export default function App() {
   useNostrUser()
-  const userPubkey = useStore(state => state.userPubkey);
-  console.log(userPubkey);
+  const userPubkey = useStore(state => state.userPubkey)
   return (
     <View style={styles.container}>
       <Image source={require('./assets/sqlogo-t.png')} style={styles.image} resizeMode="contain" />
+      {userPubkey && <Text style={styles.text}>{nip19.npubEncode(userPubkey)}</Text>}
       <StatusBar style="light" />
     </View>
   );
@@ -30,5 +33,10 @@ const styles = StyleSheet.create({
   text: {
     color: '#fff',
     fontFamily: 'Courier New',
+    fontSize: 18,
+    paddingTop: 20,
+    paddingHorizontal: 20,
+    textAlign: 'center',
+    fontWeight: 'bold'
   }
 });
