d3f4ede777e40b0fb1c1e4ef70913e183e4de081
Christopher David <chris@openagents.com>
2024-08-29 23:02:14 -0500

    Set up mobile app via React Native Expo (#1)
    * yarn create expo-app --template blank-typescript
    
    * Logo

---
+++ mobile/App.tsx
@@ -0,0 +1,28 @@
+import { StatusBar } from "expo-status-bar"
+import { Image, StyleSheet, Text, View } from "react-native"
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image source={require('./assets/sqlogo-t.png')} style={styles.image} resizeMode="contain" />
+      <StatusBar style="light" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+  text: {
+    color: '#fff',
+    fontFamily: 'Courier New',
+  }
+});
