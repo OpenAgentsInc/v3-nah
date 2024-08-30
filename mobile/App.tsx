b0d4db0b726742b85cddd1aad83372ccf0148845
Christopher David <chris@openagents.com>
2024-08-29 22:59:41 -0500

    Logo

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import { Image, StyleSheet, Text, View } from "react-native"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require('./assets/sqlogo-t.png')} style={styles.image} resizeMode="contain" />
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +13,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: 200,
+    height: 200,
+  },
+  text: {
+    color: '#fff',
+    fontFamily: 'Courier New',
+  }
 });
