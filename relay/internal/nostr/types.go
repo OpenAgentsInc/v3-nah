6e331b6f2f2e673f9bbdeb305a5b2660d77878c2
Christopher David <chris@openagents.com>
2024-08-31 14:28:22 -0500

    Added ReqMessage struct to handle subscription requests in types.go.

--- relay/internal/nostr/types.go
+++ relay/internal/nostr/types.go
@@ -1,25 +1,6 @@
 package nostr
 
-type Event struct {
-	ID        string   `json:"id"`
-	PubKey    string   `json:"pubkey"`
-	CreatedAt int64    `json:"created_at"`
-	Kind      int      `json:"kind"`
-	Tags      [][]string `json:"tags"`
-	Content   string   `json:"content"`
-	Sig       string   `json:"sig"`
-}
-
-type Filter struct {
-	IDs     []string `json:"ids,omitempty"`
-	Authors []string `json:"authors,omitempty"`
-	Kinds   []int    `json:"kinds,omitempty"`
-	Tags    [][]string `json:"#e,omitempty"`
-	Since   int64    `json:"since,omitempty"`
-	Until   int64    `json:"until,omitempty"`
-	Limit   int      `json:"limit,omitempty"`
-}
-
+// ReqMessage represents a subscription request message
 type ReqMessage struct {
 	SubscriptionID string
 	Filter         Filter
