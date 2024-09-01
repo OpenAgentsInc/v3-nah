905892c24d5a2dad1fa422d25c4deaa3fb0df952
Christopher David <chris@openagents.com>
2024-09-01 00:03:04 -0500

    simplify main.go; rename to groq folder

--- relay/cmd/relay/main.go
+++ relay/cmd/relay/main.go
@@ -8,9 +8,6 @@ import (
 	"runtime"
 
 	"github.com/openagentsinc/v3/relay/internal/nip01"
-	// "github.com/openagentsinc/v3/relay/internal/nip90"
-	// "github.com/openagentsinc/v3/relay/internal/whisper"
-	// "github.com/openagentsinc/v3/relay/internal/config"
 )
 
 func init() {
@@ -30,25 +27,6 @@ func main() {
 
 	// Initialize the relay
 	relay := nip01.NewRelay()
-
-	// TODO: Load configuration
-	// config, err := config.Load()
-	// if err != nil {
-	//     log.Fatalf("Failed to load configuration: %v", err)
-	// }
-
-	// TODO: Initialize Whisper transcription service
-	// whisperService, err := whisper.NewService(config.GroqAPIKey)
-	// if err != nil {
-	//     log.Fatalf("Failed to initialize Whisper service: %v", err)
-	// }
-
-	// TODO: Register Whisper transcription service with NIP-90
-	// nip90Service := nip90.NewService()
-	// nip90Service.RegisterProvider("whisper", whisperService)
-
-	// TODO: Integrate NIP-90 service with the relay
-	// relay.RegisterNIP90Service(nip90Service)
 
 	// Start the WebSocket server
 	log.Printf("Starting relay server on %s", *addr)
--- relay/internal/groq/groq_api.go
+++ relay/internal/groq/groq_api.go
@@ -1,4 +1,4 @@
-package whisper
+package groq
 
 import (
 	"bytes"
