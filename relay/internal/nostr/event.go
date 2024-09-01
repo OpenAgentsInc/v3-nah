2799c60f759f6f47bd3555a18f034d2f0c552a25
Christopher David <chris@openagents.com>
2024-09-01 00:17:28 -0500

    Refactor event deserialization to handle varying `CreatedAt` formats in `event.go`.

--- relay/internal/nostr/event.go
+++ relay/internal/nostr/event.go
@@ -2,6 +2,8 @@ package nostr
 
 import (
 	"encoding/json"
+	"fmt"
+	"strconv"
 	"time"
 )
 
@@ -13,6 +15,34 @@ type Event struct {
 	Tags      [][]string `json:"tags"`
 	Content   string    `json:"content"`
 	Sig       string    `json:"sig"`
+}
+
+func (e *Event) UnmarshalJSON(data []byte) error {
+	type Alias Event
+	aux := &struct {
+		CreatedAt interface{} `json:"created_at"`
+		*Alias
+	}{
+		Alias: (*Alias)(e),
+	}
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+	switch v := aux.CreatedAt.(type) {
+	case float64:
+		e.CreatedAt = time.Unix(int64(v), 0)
+	case string:
+		if i, err := strconv.ParseInt(v, 10, 64); err == nil {
+			e.CreatedAt = time.Unix(i, 0)
+		} else if t, err := time.Parse(time.RFC3339, v); err == nil {
+			e.CreatedAt = t
+		} else {
+			return fmt.Errorf("invalid timestamp format: %s", v)
+		}
+	default:
+		return fmt.Errorf("invalid timestamp type: %T", v)
+	}
+	return nil
 }
 
 func (e *Event) Serialize() ([]byte, error) {
