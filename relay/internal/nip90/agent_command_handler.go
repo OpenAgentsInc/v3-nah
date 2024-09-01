ce33d79820b71d3f5ed7e0fc770fce08a175d484
Christopher David <chris@openagents.com>
2024-09-01 09:49:30 -0500

    prettier logging

--- relay/internal/nip90/agent_command_handler.go
+++ relay/internal/nip90/agent_command_handler.go
@@ -1,21 +1,24 @@
 package nip90
 
 import (
+	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/gorilla/websocket"
-	"github.com/openagentsinc/v3/relay/internal/nostr"
 	"github.com/openagentsinc/v3/relay/internal/common"
+	"github.com/openagentsinc/v3/relay/internal/nostr"
 )
 
 func HandleAgentCommandRequest(conn *websocket.Conn, event *nostr.Event) {
 	command := extractCommand(event)
 	log.Printf("Received agent command request: %s", command)
 
-	// TODO: Implement agent command routing logic here
-	// For now, we'll just echo the command back as a response
+	// Log all of the fields of the event, one per line
+	logEventDetails(event)
 
+	// TODO: Implement agent command routing logic here
 	responseEvent := &nostr.Event{
 		Kind:      6838, // Updated event kind for agent command response
 		Content:   "Acknowledged. Will respond shortly.",
@@ -29,6 +32,24 @@ func HandleAgentCommandRequest(conn *websocket.Conn, event *nostr.Event) {
 	if err != nil {
 		log.Println("Error writing agent command response to WebSocket:", err)
 	}
+}
+
+func logEventDetails(event *nostr.Event) {
+	var sb strings.Builder
+
+	sb.WriteString("Event Details:\n")
+	sb.WriteString(fmt.Sprintf("  ID: %s\n", event.ID))
+	sb.WriteString(fmt.Sprintf("  PubKey: %s\n", event.PubKey))
+	sb.WriteString(fmt.Sprintf("  CreatedAt: %s\n", event.CreatedAt))
+	sb.WriteString(fmt.Sprintf("  Kind: %d\n", event.Kind))
+	sb.WriteString("  Tags:\n")
+	for _, tag := range event.Tags {
+		sb.WriteString(fmt.Sprintf("    - %v\n", tag))
+	}
+	sb.WriteString(fmt.Sprintf("  Content: %s\n", event.Content))
+	sb.WriteString(fmt.Sprintf("  Sig: %s\n", event.Sig))
+
+	log.Print(sb.String())
 }
 
 func extractCommand(event *nostr.Event) string {
